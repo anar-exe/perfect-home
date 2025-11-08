99f406573821d33e3cbdfc163f665609a791e51d
Elliot Alderson <anarq2006q@proton.me>
2025-11-09 02:22:25 +0400

    latest

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-window.addEventListener("scroll", function() {
-  const navbar = document.querySelector(".navbar"); // navbar elementi
-  const scrollY = window.scrollY; // hal-hazırkı scroll miqdarı (piksel ilə)
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar");
+  const scrollY = window.scrollY;
 
-  if (scrollY > 100) { // 100px aşağı scroll ediləndə
+  if (scrollY > 100) {
     navbar.classList.add("scroll");
   } else {
     navbar.classList.remove("scroll");
   }
 });
+
+const navbarmenu = document.querySelector(".menu")
+const navbar = document.querySelector("nav")
+navbarmenu.addEventListener("click", () => (
+  navbar.classList.toggle('menu-toggle')
+))
